c6000366fe8c4b3bda0b9a4690696503b0e39597
Lombeira <glauco.lomenha@memed.com.br>
2022-10-22 20:07:21 -0300

    changing workflow settings

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./styles/global.css"
 export function App() {
 
   return (
-    <h1 className="font-bold text-2xl text-violet-600">01:01</h1>
+    <h1 className="font-bold text-2xl text-violet-600">26:00</h1>
   )
 }
 
